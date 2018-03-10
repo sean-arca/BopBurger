0542477cabdaad7fa005d14c00a892199d53bc6b
Sean Arca <sean.arca@northwestern.edu>
2018-03-10 17:52:03 -0600

    added mysql connection

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+// Dependencies
+var mysql = require("mysql");
+
+// MySQL Connection Info
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Lollersk8!",
+    database: "burgers_db"
+});
+
+// Create the Connection to the DB
+connection.connect(function (err) {
+    if (err) {
+        throw err;
+    };
+
+    console.log('\nMySQL: Connected to Burger database as id ' + connection.threadId + '\n');
+})
