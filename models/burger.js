700a82e75ef1fcccd0a97c482a6ca14b531ba0dc
Sean Arca <sean.arca@northwestern.edu>
2018-03-10 22:50:03 -0600

    added burger model

---
+++ models/burger.js
@@ -0,0 +1,30 @@
+// Dependencies
+var orm = require("../config/orm");
+
+// Burger Object
+var burger = {
+    
+    // Select All
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
+
+    // Insert One Burger
+    insertOneBurger: function (cols, vals, cb) {
+        orm.insertOneBurger("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+
+    // Update One Burger
+    updateOneBurger: function (objVals, condition, cb) {
+        orm.updateOneBurger("burgers", objVals, condition, function (res) {
+            cb(res);
+        });
+    }
+};
+
+// Export module
+module.exports = burger;
