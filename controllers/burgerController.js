7c26cd4960c7ab2bd64e3d9d0323a6a9ed1aac64
Sean Arca <sean.arca@northwestern.edu>
2018-03-11 00:06:30 -0600

    added server js and change controller

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -10,16 +10,14 @@ router.get("/", function (req, res) {
     });
 });
 
-router.post("/", function (req, res) {
+router.post("/burgers", function (req, res) {
     burger.insertOneBurger(["burger_name"], [req.body.burger_name], function (data) {
         res.json(data);
     });
 });
 
-router.put("/:id", function (req, res) {
+router.put("/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
-
-    console.log("condition", condition);
 
     burger.updateOneBurger({ devoured: true }, condition, function (data) {
         res.json(data);
