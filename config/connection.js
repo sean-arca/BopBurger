4a0322dccfb18b5d01dadefbc5cc1938c6799184
Sean Arca <sean.arca@northwestern.edu>
2018-03-12 11:10:59 -0500

    linked to JAWSDB on connection

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,19 @@
 var mysql = require("mysql");
 
 // MySQL Connection Info
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Lollersk8!",
-    database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Lollersk8!",
+        database: "burgers_db"
+    });
+};    
 
 // Create the Connection to the DB
 connection.connect(function (err) {
